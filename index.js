32cb2a8aa68d10e7c14d4c301f3a75e160a5f0a4
thisisshub <thisisshub@gmail.com>
2020-01-05 19:32:04 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 ;(function() {
-    var slider = tns({
-      container: '.projects-slider',
-      items: 1,
-      gutter: 20,
-      center: true,
-      nav: false,
-      autoplay: true,
-      controls: false,
-      edgePadding: 20,
-      responsive: {
-        1200: {
-          items: 3
-        }
+  var slider = tns({
+    container: '.projects-slider',
+    items: 1,
+    gutter: 20,
+    center: true,
+    nav: false,
+    autoplay: true,
+    controls: false,
+    edgePadding: 20,
+    responsive: {
+      1200: {
+        items: 3
       }
-    });
-  
-  }());
-  
+    }
+  });
+
+}());
