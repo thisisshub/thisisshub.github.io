51dcc8c12b6281766d02606195122fe70a44e0b7
thisisshub <thisisshub@gmail.com>
2020-01-05 19:29:45 +0530

    added blog

---
+++ index.js
@@ -0,0 +1,19 @@
+;(function() {
+    var slider = tns({
+      container: '.projects-slider',
+      items: 1,
+      gutter: 20,
+      center: true,
+      nav: false,
+      autoplay: true,
+      controls: false,
+      edgePadding: 20,
+      responsive: {
+        1200: {
+          items: 3
+        }
+      }
+    });
+  
+  }());
+  
